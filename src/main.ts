c66f0fe2864d213c78d913847f3efae290d95e03
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-05-06 11:51:39 +0700

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
   const serverConfig: Record<string, any> = config.get('server');
   const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule);
 
-  console.log(serverConfig);
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
+  }
 
   const port = process.env.PORT || serverConfig.port;
-  const app = await NestFactory.create(AppModule);
   await app.listen(port);
-  logger.log(`Application is up and running in port ${port}`);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
